6ef35c6d4782e0fe8c912f9f7428c542bf01d534
spindouken <mlc9181209@gmail.com>
2023-02-06 20:19:26 -0600

    add test files (storage),restructure files/folders

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""testing file storage module"""
+import unittest
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+
+class TestFileStorage(unittest.TestCase):
+    """placeholder text"""
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_all(self):
+        pass
+
+    def test_new(self):
+        pass
+
+    def test_save(self):
+        pass
+
+    def test_reload(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
