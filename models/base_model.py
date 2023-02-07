ccef883e5f491f21a101d83779c0f2c780c3b02a
spindouken <mlc9181209@gmail.com>
2023-02-06 19:24:41 -0600

    permissions, initial task 6 upload

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """placeholder text"""
 from datetime import datetime
 import uuid
+import models
 
 
 class BaseModel:
@@ -17,16 +18,18 @@ class BaseModel:
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """placeholder"""
-        based = self.__class__.__name__
-        return f"[{based}] ({self.id}) {self.__dict__}"
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """placeholder"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """placeholder"""
