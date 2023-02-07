55f4e912d64878081871947c7416fb493db2ee87
spindouken <mlc9181209@gmail.com>
2023-02-06 20:58:36 -0600

    reformat change over 9000

--- models/base_model.py
+++ models/base_model.py
@@ -25,7 +25,7 @@ class BaseModel:
 
     def __str__(self):
         """placeholder"""
-        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """placeholder"""
