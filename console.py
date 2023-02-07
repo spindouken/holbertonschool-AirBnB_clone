773170d74954af36898b2105326478a5603ca9e0
jbees918 <5590@holbertonstudents.com>
2023-02-07 06:04:48 +0000

    docchange

--- console.py
+++ console.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""module containing code for command interpreter"""
+"""module containing the code for command interpreter"""
 
 import cmd
 	from models.base_model import BaseModel
