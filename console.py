82dc9fb838021f726b04f2f5631b126529b25f66
jbees918 <5590@holbertonstudents.com>
2023-02-07 01:45:21 +0000

    console fix

---
+++ console.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""based console"""
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """based command interpreter for BNB clone"""
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """You're just going to quit, eh?!"""
+        return True
+    def do_EOF(self, arg):
+        """Very well, goodbye!"""
+        return True
+    def do_create(self, arg):
+	pass
+
+    def do_show(self, arg):
+	pass
+
+    def do_destroy(self, arg):
+	pass
+
+    def do_destroy(self, arg):
+	pass
+
+    def do_all(self, arg):
+	pass
+
+    def do_update(self, arg):
+	pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
