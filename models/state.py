511f7eeeb855cb4139a2b6ad5ae146b8146a5e2b
jbees918 <5590@holbertonstudents.com>
2023-02-07 21:52:41 +0000

    added class state

---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module containing the subclass State.
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    State class reps state & is a subclass of BaseModel 
+    """
+    name = ""
