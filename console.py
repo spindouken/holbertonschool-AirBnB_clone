523cb2e735f6c10340491037f2b40c6f5b2ddc56
spindouken <107735231+spindouken@users.noreply.github.com>
2023-02-07 18:24:22 -0600

    Update console.py

--- console.py
+++ console.py
@@ -2,15 +2,14 @@
 """module containing the code for command interpreter"""
 
 import cmd
-	from models.base_model import BaseModel
-	from models.user import User
-	from models.state import State
-	from models.city import City
-	from models.amenity import Amenity
-	from models.place import Place
-	from models.review import Review
-	from models import storage
-	import re
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models import storage
 
 
 	valid_class = {
@@ -40,22 +39,98 @@ class HBNBCommand(cmd.Cmd):
         return False
 
     def do_create(self, arg):
-        pass
+        args = shlex.split(args)
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        class_name = args[0]
+        if class_name not in models.classes:
+            print("** class doesn't exist **")
+            return
+        obj = models.classes[class_name]()
+        obj.save()
+        print(obj.id)
 
     def do_show(self, arg):
-        pass
+        args = shlex.split(args)
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        class_name = args[0]
+        if class_name not in models.classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) == 1:
+            print("** instance id missing **")
+            return
+        obj_id = args[1]
+        obj = models.storage.all().get(class_name + "." + obj_id)
+        if obj is None:
+            print("** no instance found **")
+            return
+        print(obj)
 
     def do_destroy(self, arg):
-        pass
-
-    def do_destroy(self, arg):
-        pass
+        args = shlex.split(args)
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        class_name = args[0]
+        if class_name not in models.classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        obj_id = args[1]
+        obj = models.storage.all().get(class_name + "." + obj_id)
+        if obj is None:
+            print("** no instance found **")
+            return
+        del obj
+        storage.save()
+        return
 
     def do_all(self, arg):
-        pass
+        args = line.split()
+        all_objects = storage.all()
+        if len(args) == 0:
+            print([str(obj) for obj in all_objects.values()])
+        elif args[0] in models.classes:
+            print([str(obj) for obj in all_objects.values()
+                if type(obj).__name__ == args[0]])
+        else:
+            print("** class doesn't exist **")
 
     def do_update(self, arg):
-        pass
+        args = line.split()
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        if args[0] not in models.classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) == 1:
+            print("** instance id missing **")
+            return
+        obj_id = args[1]
+        if obj_id not in storage.all():
+            print("** no instance found **")
+            return
+        obj = storage.all()[obj_id]
+        if len(args) == 2:
+            print("** attribute name missing **")
+            return
+        if len(args) == 3:
+            print("** value missing **")
+            return
+        attr_name = args[2]
+        if attr_name in ["id", "created_at", "updated_at"]:
+            print("** cannot update id, created_at, and updated_at attributes **")
+            return
+        attr_value = args[3].strip('"')
+        setattr(obj, attr_name, type(getattr(obj, attr_name))(attr_value))
+        obj.save()
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
