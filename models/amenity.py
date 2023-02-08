9674bed798c1d99c6823bc93a2fe7925c0a717a9
spindouken <mlc9181209@gmail.com>
2023-02-07 19:30:38 -0600

    added amenity file, hopefully resolve conflicts

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module for subclass: Amenity"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Class 'Amenity' that inherits from BaseModel"""
+    name = ""
